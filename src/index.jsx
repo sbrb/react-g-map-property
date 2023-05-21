127327d833d453b5a09838693bce11b92e3646ac
sbrb <info.sbriju@gmail.com>
2023-05-21 11:16:37 +0530

    H

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
-import ReactDOM from "react-dom";
 import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
